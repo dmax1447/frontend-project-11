299f464054c138d04464ed1728f8549b094e9071
Max Dragaytsev <40897499+dmax1447@users.noreply.github.com>
2023-05-15 16:27:52 +0300

    step 7 - change messages

--- src/locales/ru.js
+++ src/locales/ru.js
@@ -1,12 +1,12 @@
 export default {
   feedback_messages: {
-    required: 'Обязательное поле',
-    url_exist: 'Такой URL уже добавлен',
-    url_added: 'URL успешно добавлен',
+    required: 'Не должно быть пустым',
+    url_exist: 'RSS уже существует',
+    url_added: 'RSS успешно загружен',
     url_invalid: 'Ссылка должна быть валидным URL',
-    fetch_error: 'Источник не содержит валидный RSS',
-    network_error: 'Ошибка выполнения запроса',
-    http_error: 'Ресурс не найден в запрошенном урл',
-    generic_network_error: 'Сбой - неизвестная ошибка сети',
+    fetch_error: 'Ресурс не содержит валидный RSS',
+    network_error: 'Ошибка сети',
+    http_error: 'Ошибка сети',
+    generic_network_error: 'Ошибка сети',
   },
 };
